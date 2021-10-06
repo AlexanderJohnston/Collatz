20c1d689e52d5325b9acede1406ff6c403a1a73d
Alexander Johnston <alexanderj.roaming@gmail.com>
2021-10-06 12:43:34 -0400

    COLLATZ-01: Initial collatz commit.

---
+++ Collatz/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collatz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" How many numbers to test on Collatz?");
+            var test = Console.ReadLine();
+            var numberTries = Int32.Parse(test);
+            for (int i = 1; i < numberTries; i++)
+            {
+                double n = i;
+                var sequence = new List<double>();
+                while (n != 4)
+                {
+                    n = NextCollatz(n);
+                    sequence.Add(n);
+                }
+                // Final sequence of all runs.
+                sequence.Add(2);
+                sequence.Add(1);
+
+                Console.Write($"Collatz {i}: ");
+                foreach (var integer in sequence)
+                {
+                    Console.Write($"{integer},");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+        static double NextCollatz(double n)
+        {
+            var sequence = n % 2;
+            return sequence switch
+            {
+                0.0d => n / 2,
+                1.0d => (3 * n) + 1,
+                _ => throw new NotImplementedException()
+            };
+        }
+
+    }
+}
