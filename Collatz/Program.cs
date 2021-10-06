69055bbe0e54569a38f6a25399208b0938ecbe7e
Alexander Johnston <alexanderj.roaming@gmail.com>
2021-10-06 12:50:58 -0400

    REWRITE-01: Rewrite the collatz function.

--- Collatz/Program.cs
+++ Collatz/Program.cs
@@ -7,30 +7,68 @@ namespace Collatz
     {
         static void Main(string[] args)
         {
+            RunAndAsk();
+            while (Console.ReadLine() == string.Empty)
+            {
+                RunAndAsk();
+            }
+        }
+
+        private static void RunAndAsk()
+        {
+            CalculateCollatz();
+            Console.WriteLine("Run again?");
+        }
+
+        private static void CalculateCollatz()
+        {
+            int numberTries = AskUserForMaximumInteger();
+            for (int i = 1; i <= numberTries; i++)
+            {
+                var sequence = GetCollatzSequence(i);
+                WriteCollatz(i, sequence);
+            }
+        }
+
+        private static List<double> GetCollatzSequence(int i)
+        {
+            List<double> sequence = FindUniquePortion(i);
+            // Final sequence of all runs.
+            sequence.Add(2);
+            sequence.Add(1);
+
+            return sequence;
+        }
+
+        private static void WriteCollatz(int i, List<double> sequence)
+        {
+            Console.Write($"Collatz {i}: ");
+            foreach (var integer in sequence)
+            {
+                Console.Write($"{integer},");
+            }
+            Console.WriteLine();
+        }
+
+        private static List<double> FindUniquePortion(int i)
+        {
+            double n = i;
+            var sequence = new List<double>();
+            while (n != 4)
+            {
+                n = NextCollatz(n);
+                sequence.Add(n);
+            }
+
+            return sequence;
+        }
+
+        private static int AskUserForMaximumInteger()
+        {
             Console.WriteLine(" How many numbers to test on Collatz?");
             var test = Console.ReadLine();
             var numberTries = Int32.Parse(test);
-            for (int i = 1; i < numberTries; i++)
-            {
-                double n = i;
-                var sequence = new List<double>();
-                while (n != 4)
-                {
-                    n = NextCollatz(n);
-                    sequence.Add(n);
-                }
-                // Final sequence of all runs.
-                sequence.Add(2);
-                sequence.Add(1);
-
-                Console.Write($"Collatz {i}: ");
-                foreach (var integer in sequence)
-                {
-                    Console.Write($"{integer},");
-                }
-                Console.WriteLine();
-            }
-            Console.ReadLine();
+            return numberTries;
         }
 
         static double NextCollatz(double n)
